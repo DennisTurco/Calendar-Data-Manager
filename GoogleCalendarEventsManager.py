53cf5ed3a66f78e965977ed2203319520fe84094
DennisTurco <dennisturco@gmail.com>
2023-11-06 18:48:30 +0100

    feat: first version

--- GoogleCalendarEventsManager.py
+++ GoogleCalendarEventsManager.py
@@ -1,18 +1,23 @@
 import os.path
 
 from typing import List, Set, Tuple, Dict
-import datetime as dt
+import datetime
 
 from google.auth.transport.requests import Request
 from google.oauth2.credentials import Credentials
 from google_auth_oauthlib.flow import InstalledAppFlow
 from googleapiclient.discovery import build
 from googleapiclient.errors import HttpError
+import requests
 
 class GoogleCalendarEventsManager:
     
-    data: Dict[str, List[str]] = None
+    SCOPE = ["https://www.googleapis.com/auth/calendar"]
+        
+    def __init__():
+        pass
     
+    @staticmethod
     def saveDataToFile(data: Dict[str, List[str]], filepath: str, delimeter: str = "|", encodingType: str = None):
         if filepath == None or len(filepath) == 0: Exception("File path can't be null")
         
@@ -28,6 +33,7 @@ class GoogleCalendarEventsManager:
             counter += 1
         file.close()
     
+    @staticmethod
     def loadDataFromFile(filepath: str, delimeter: str = "|") -> Dict[str, List[str]]:
         if filepath == None or len(filepath) == 0: Exception("File path can't be null")
         
@@ -41,7 +47,8 @@ class GoogleCalendarEventsManager:
         file.close()
         return data
     
-    def connectionSetup(token_path: str, credentials_path: str, scopes: str) -> Credentials:
+    @staticmethod
+    def connectionSetup(credentials_path: str, scopes: str, token_path: str) -> Credentials:
         if token_path == None or len(token_path) == 0: Exception("Token path can't be empty")
         if credentials_path == None or len(credentials_path) == 0: Exception("Credentials path can't be empty")
         if scopes == None or len(scopes) == 0: Exception("Scopes link can't be empty")
@@ -53,22 +60,63 @@ class GoogleCalendarEventsManager:
         
         if not credentials or not credentials.valid:
             if credentials and credentials.expired and credentials.refresh_token:
-                credentials.refresh(Request())
+                credentials.refresh(Request())  # Refresh the token
             else:
-                flow = InstalledAppFlow.from_client_secrets_file(credentials_path, scopes)
-                credentials = flow.run_local_server(port=0)
+                try:
+                    flow = InstalledAppFlow.from_client_secrets_file(credentials_path, scopes)
+                    credentials = flow.run_local_server(port=0)
+                except:
+                    return None
                 
             with open(token_path, "w") as token:
                 token.write(credentials.to_json())
+        
+        return credentials
     
+    # TODO: Test
+    @staticmethod
+    def refreshToken():
+        client_secret = "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"
+        client_id = "629129916032-161pnnbejkg238auc0rethmlg1njc6om.apps.googleusercontent.com"
+        refresh_token = "1//09PbgtBFQPy8dCgYIARAAGAkSNwF-L9IreGgrAtTftppccc4ClFOpbEBq3G6rAJ11uUbvX8roppBgsrvHXBx88QEn5pJh2A5Nols"
+        token_url = 'https://oauth2.googleapis.com/token'
+        
+        data = {
+            'client_id': client_id,
+            'client_secret': client_secret,
+            'refresh_token': refresh_token,
+            'grant_type': 'refresh_token'
+        }
+
+        response = requests.post(token_url, data=data)
+
+        if response.status_code == 200:
+            token_data = response.json()
+            new_access_token = token_data['access_token']
+            new_refresh_token = token_data.get('refresh_token', refresh_token)
+
+            # Use the new access token for Google Calendar API requests.
+            print(new_access_token)
+            print(new_refresh_token)
+        else:
+            print("Error refreshing access token:", response.text)
+    
+    #TODO: ha senso??
+    @staticmethod
     def addData(data: Dict[str, List[str]], ID: str, data_list: List[str]) -> bool:
+        if ID == None: Exception("ID can't be null")
+        
         if ID not in data:
             data[ID] = data_list
             return True
         else:
             return False
 
-    def createEvent(service, title_event: str, start_date, end_date, color_event_id: int = 1, timeZone: str = ''):
+    @staticmethod
+    def createEvent(creds: Credentials, title_event: str, start_date: datetime, end_date: datetime, color_event_id: int = 1, timeZone: str = 'UTC'):
+        if creds == None: Exception("Credentials can't be null")
+        service = build("calendar", "v3", credentials=creds)
+        
         # set the event
         event = {
             'summary': title_event,
@@ -86,39 +134,259 @@ class GoogleCalendarEventsManager:
         # create the event
         service.events().insert(calendarId='primary', body=event).execute()
     
-    def getTitleByID(service, event, ID: str) -> str:
+    @staticmethod
+    def getTitleByID(creds: Credentials, ID: str) -> str:
+        if creds == None: Exception("Credentials can't be null")
         if ID == None: Exception("ID can't be null")
-        pass
+        
+        try:
+            # Call the Google Calendar API to get the event by ID
+            service = build("calendar", "v3", credentials=creds)
+            event = service.events().get(calendarId='primary', eventId=ID).execute()
+            
+            title = event['summary'] # Extract and return the event title
+            return title
+        except Exception as e:
+            print(f"An error occurred: {str(e)}")
+            return None
     
-    def getTitleByDate(service, event, start_date: str, end_date: str) -> str:
+    @staticmethod
+    def getTitleByDate(creds: Credentials, start_date: str, end_date: str) -> str:
+        if creds == None: Exception("Credentials can't be null")
         if start_date == None or end_date == None: Exception("Date can't be empty")
-        pass
+        
+        try:
+            service = build("calendar", "v3", credentials=creds)
+            events_result = service.events().list(
+                calendarId='primary',
+                timeMin=start_date,
+                timeMax=end_date,
+                maxResults=1,
+                singleEvents=True,
+                orderBy='startTime'
+            ).execute()
+
+            events = events_result.get('items', [])
+
+            if events:
+                event = events[0]
+                title = event.get('summary', '')
+                return title
+            else:
+                return None
+        except Exception as e:
+            print(f"An error occurred: {str(e)}")
+            return None
     
-    def getIDByTitle(service, event, title: str) -> List:
+    @staticmethod
+    def getIDByTitle(creds: Credentials, title: str) -> str:
+        if creds == None: Exception("Credentials can't be null")
         if title == None: Exception("Title can't be empty")
-        pass
+        
+        try:
+            service = build("calendar", "v3", credentials=creds)
+            events_result = service.events().list(
+                calendarId='primary',
+                q=title,
+                maxResults=1,
+                singleEvents=True,
+            ).execute()
+            
+            events = events_result.get('items', [])
+            
+            if events:
+                event = events[0]
+                ID = event['id']
+                return ID
+            else:
+                return None  
+        except Exception as e:
+            print(f"An error occurred: {str(e)}")
+            return None
     
-    def getIDByDate(service, event, start_date: str, end_date: str) -> List:
+    @staticmethod
+    def getIDByDate(creds: Credentials, start_date: str, end_date: str) -> List:
+        if creds == None: Exception("Credentials can't be null")
         if start_date == None or end_date == None: Exception("Date can't be empty")
-        pass
+        
+        try:
+            service = build("calendar", "v3", credentials=creds)
+            events_result = service.events().list(
+                calendarId='primary',
+                timeMin=start_date,
+                timeMax=end_date,
+                maxResults=1,
+                singleEvents=True,
+                orderBy='startTime'
+            ).execute()
+
+            events = events_result.get('items', [])
+
+            if events:
+                event = events[0]
+                ID = event['id']
+                return ID
+            else:
+                return None
+        except Exception as e:
+            print(f"An error occurred: {str(e)}")
+            return None
     
-    def getDescriptionByID(service, event, ID: str) -> str:
+    @staticmethod
+    def getDescriptionByID(creds: Credentials, ID: str) -> str:
+        if creds == None: Exception("Credentials can't be null")
         if ID == None: Exception("ID can't be null")
-        pass
+        
+        try:
+            # Call the Google Calendar API to get the event by ID
+            service = build("calendar", "v3", credentials=creds)
+            event = service.events().get(calendarId='primary', eventId=ID).execute()
+            
+            description = event.get('description') # Extract and return the event description
+            return description
+        except Exception as e:
+            print(f"An error occurred: {str(e)}")
+            return None
     
-    def getAllDescriptionsByTitle(service, event, like_mode: bool = False) -> Dict[str, List[str]]:
-        pass
+    # TODO: test 
+    # TODO: add like mode
+    @staticmethod
+    def getAllDescriptionsByTitle(creds: Credentials, title: str, like_mode: bool = False) -> Dict[str, List[str]]:
+        if creds == None: Exception("Credentials can't be null")
+        if title == None: Exception("Title can't be null")
+        
+        try:
+            service = build("calendar", "v3", credentials=creds)
+            events = []
+            
+            now = datetime.datetime.now().isoformat() + "Z"
+            start_date_search = None 
+            end_date_search = None
+            while (True):
+                # set the events list
+                events_result = service.events().list(
+                    calendarId="primary", 
+                    maxResults=500, 
+                    timeMin=start_date_search, 
+                    timeMax=now, 
+                    singleEvents=True, 
+                    orderBy="startTime").execute()
+                
+                events = events + events_result.get('items', [])
+                
+                # i quit if i found all the events -> it happens when the date of the last element inside the list is the same for two times
+                if events[len(events)-1]['end'].get('dateTime') == end_date_search:
+                    break
+
+                end_date_search = events[len(events)-1]['end'].get('dateTime')
+                
+                start_date_search = end_date_search
+            
+            if events:
+                return events
+            else:
+                return None  
+        except Exception as e:
+            print(f"An error occurred: {str(e)}")
+            return None
     
-    def editEventTitleByID(service, event, ID: str):
+    # TODO: test me 
+    @staticmethod
+    def editEventTitleByID(creds: Credentials, ID: str, title: str):
+        if creds == None: Exception("Credentials can't be null")
         if ID == None: Exception("ID can't be null")
-        pass
+        if title == None: Exception("Title can't be empty")
+        
+        try:
+            service = build("calendar", "v3", credentials=creds)
+            event = service.events().get(calendarId='primary', eventId=ID).execute()
+            event['summary'] = title 
+        except Exception as e:
+            print(f"An error occurred: {str(e)}")
     
-    def editEventDateByID(service, event, ID: str):
+    # TODO: test me
+    @staticmethod
+    def editEventDateByID(creds: Credentials, ID: str, start_date: datetime, end_date: datetime, timeZone: str = 'UTC'):
+        if creds == None: Exception("Credentials can't be null")
         if ID == None: Exception("ID can't be null")
-        pass
+        if start_date == None or end_date == None: Exception("Date can't be empty")
+        
+        try:
+            service = build("calendar", "v3", credentials=creds)
+            event = service.events().get(calendarId='primary', eventId=ID).execute()
+            
+            # Update the start and end times along with the time zone
+            event['start'] = {'dateTime': start_date, 'timeZone': timeZone}
+            event['end'] = {'dateTime': end_date, 'timeZone': timeZone}
+
+            # Update the event in the calendar
+            service.events().update(
+                calendarId='primary',
+                eventId=ID,
+                body=event
+            ).execute()
+        
+        except Exception as e:
+            print(f"An error occurred: {str(e)}")
     
-    def editDescriptionEventByID(service, event):
-        pass
+    # TODO: test me 
+    @staticmethod
+    def editDescriptionEventByID(creds: Credentials, ID: str, description: str):
+        if creds == None: Exception("Credentials can't be null")
+        if ID == None: Exception("ID can't be null")
+        if description == None: Exception("Description can't be null")
+        
+        try:
+            service = build("calendar", "v3", credentials=creds)
+            event = service.events().get(calendarId='primary', eventId=ID).execute()
+            
+            # Update the descritpion
+            event['description'] = description
+
+            # Update the event in the calendar
+            service.events().update(
+                calendarId='primary',
+                eventId=ID,
+                body=event
+            ).execute()
+        
+        except Exception as e:
+            print(f"An error occurred: {str(e)}")
     
-    def editDescriptionEventByIDTitle(service, event, like_mode: bool = False):
-        pass
+    # TODO: test me 
+    # TODO: add like mode
+    @staticmethod
+    def editDescriptionEventsByTitle(creds: Credentials, title: str, description: str, like_mode: bool = False):
+        if creds == None: Exception("Credentials can't be null")
+        if title == None: Exception("Title can't be null")
+        if description == None: Exception("Description can't be null")
+        
+        try:
+            service = build("calendar", "v3", credentials=creds)
+            events = service.events().get(calendarId='primary', summary=title).execute()
+            
+            for event in events:
+                # Update the description
+                event['description'] = description
+
+                # Update the event in the calendar
+                service.events().update(
+                    calendarId='primary',
+                    eventId=event['id'],
+                    body=event
+                ).execute()
+        
+        except Exception as e:
+            print(f"An error occurred: {str(e)}")
+
+    @staticmethod
+    def deleteEventByID(creds: Credentials, ID: str):
+        if creds == None: Exception("Credentials can't be null")
+        if ID == None: Exception("ID can't be null")
+
+        service = build('calendar', 'v3', credentials=creds)
+
+        try:
+            service.events().delete(calendarId='primary', eventId=ID).execute()
+        except Exception as e:
+            print(f"An error occurred: {str(e)}")
